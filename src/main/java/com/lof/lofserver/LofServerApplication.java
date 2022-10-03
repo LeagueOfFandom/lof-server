df0b720d8e8ae4bd1c644b1fab2594c5cb9ca0db
imygnam <imygnam@gmail.com>
2022-10-03 13:26:59 +0900

    기본

---
+++ src/main/java/com/lof/lofserver/LofServerApplication.java
@@ -0,0 +1,13 @@
+package com.lof.lofserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LofServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LofServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lof/lofserver/LofServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lof.lofserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LofServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
