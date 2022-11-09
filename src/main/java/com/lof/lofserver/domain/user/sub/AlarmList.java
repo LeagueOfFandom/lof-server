5fa66d280a6bdf7290a4402b3f6c4245956d194e
imygnam <imygnam@gmail.com>
2022-11-09 14:23:51 +0900

    create alarmList

---
+++ src/main/java/com/lof/lofserver/domain/user/sub/AlarmList.java
@@ -0,0 +1,12 @@
+package com.lof.lofserver.domain.user.sub;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+
+public record AlarmList(String title, String content, LocalDateTime createdAt) {
+    @Builder
+    public AlarmList {
+    }
+}
