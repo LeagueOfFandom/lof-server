f3bcf1ddb9482b0a977ae9c959956d286f621d63
imygnam <imygnam@gmail.com>
2022-11-12 12:46:50 +0900

    pinpoint ip 대로

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 REPOSITORY=/home/app
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -fla java | awk '{print $1}')
+IP=$(hostname -I)
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -17,4 +18,4 @@ echo "> JAR NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
-nohup /opt/jdk-17/bin/java -jar -javaagent:/home/ec2-user/pinpoint-agent-2.4.2/pinpoint-bootstrap-2.4.2.jar -Dpinpoint.agentId=api-server1 -Dpinpoint.applicationName=API-SERVER -Dpinpoint.config=/home/ec2-user/pinpoint-agent-2.4.2/pinpoint-root.config -Dspring.profiles.active=devServer $JAR_NAME 1>$REPOSITORY/lof.log 2>&1 &
+nohup /opt/jdk-17/bin/java -jar -javaagent:/home/ec2-user/pinpoint-agent-2.4.2/pinpoint-bootstrap-2.4.2.jar -Dpinpoint.agentId="$IP" -Dpinpoint.applicationName=API-SERVER -Dpinpoint.config=/home/ec2-user/pinpoint-agent-2.4.2/pinpoint-root.config -Dspring.profiles.active=devServer "$JAR_NAME" 1>$REPOSITORY/lof.log 2>&1 &
