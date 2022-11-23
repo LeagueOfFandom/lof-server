f195a9fea989834284f9b2ffae83bfb24107951c
imygnam <imygnam@gmail.com>
2022-11-23 12:12:01 +0900

    banner view fix

--- src/main/java/com/lof/lofserver/service/community/CommunityServiceImpl.java
+++ src/main/java/com/lof/lofserver/service/community/CommunityServiceImpl.java
@@ -12,9 +12,7 @@ public class CommunityServiceImpl implements CommunityService {
     @Override
     public BannerView getBannerList() {
         List<String> bannerList = new ArrayList<>();
-        bannerList.add("https://d654rq93y7j8z.cloudfront.net/lof_banner/1.jpg");
-        bannerList.add("https://d654rq93y7j8z.cloudfront.net/lof_banner/2.jpg");
-        bannerList.add("https://d654rq93y7j8z.cloudfront.net/lof_banner/3.jpg");
+        bannerList.add("https://event.img.afreecatv.com/esports/news/fomos/2021/08/29/2965612a5d855f259.jpg");
 
         return new BannerView(bannerList);
     }
--- src/main/java/com/lof/lofserver/service/match/response/MatchView.java
+++ src/main/java/com/lof/lofserver/service/match/response/MatchView.java
@@ -14,6 +14,7 @@ public record MatchView(
         Boolean isAlarm,
         Long homeScore,
         Long awayScore,
+        String videoLink,
         String status
 ) {
     @Builder
