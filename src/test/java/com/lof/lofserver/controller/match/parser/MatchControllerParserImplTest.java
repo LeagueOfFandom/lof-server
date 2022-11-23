183d21b4b9483a30447628337261211b5f5dac74
imygnam <imygnam@gmail.com>
2022-11-23 12:46:41 +0900

    main view change test

--- src/test/java/com/lof/lofserver/controller/match/parser/MatchControllerParserImplTest.java
+++ src/test/java/com/lof/lofserver/controller/match/parser/MatchControllerParserImplTest.java
@@ -19,19 +19,7 @@ class MatchControllerParserImplTest {
     @InjectMocks
     MatchControllerParserImpl matchControllerParser;
 
-    @Test
-    @DisplayName("매치 정보 파싱 테스트")
-    void parseMatchInfo(){
-        //given
-        List<Object> objectList = createObjectListTest();
 
-        //when
-        MainPageResponse mainPageResponse = matchControllerParser.parseObjectListToMainPageResponse(objectList);
-
-        //then
-        assertThat(mainPageResponse.commonItemListResponse().get(0).viewType()).isEqualTo("HOME_MATCH_TITLE_VIEW");
-        assertThat(mainPageResponse.commonItemListResponse().get(0).viewObject()).isInstanceOf(TextArrowView.class);
-    }
 
     private List<Object> createObjectListTest() {
         List<Object> objectList = new ArrayList<>();
